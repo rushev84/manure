3cbcda111ccd57b4dfd431b055837e0f150d5e03
SergeyRushev <rushev84@yandex.ru>
2022-05-17 09:53:12 +0300

    import as collection, console command for import

---
+++ app/Console/Commands/ExcelImportManures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Imports\ManuresImport;
+use Illuminate\Console\Command;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ExcelImportManures extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'excel:import:manures';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Imports list of manures from excel-file';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $filePath = public_path().'/imports/manures.xlsx';
+        Excel::import(new ManuresImport(), $filePath);
+        $this->info('Imported successful!');
+    }
+}
--- app/Imports/ManuresImport.php
+++ app/Imports/ManuresImport.php
@@ -3,28 +3,27 @@
 namespace App\Imports;
 
 use App\Models\Manure;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
 
-class ManuresImport implements ToModel
+class ManuresImport implements ToCollection
 {
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
+    public function collection(Collection $collection)
     {
-        return new Manure([
-            'name' => $row[0],
-            'norm_nitrogen' => $row[1],
-            'norm_phosphorus' => $row[2],
-            'norm_potassium' => $row[3],
-//            // !!! сделать так, чтобы юзер мог писать название культуры, а не её айдишник
-            'culture_id' => $row[4],
-            'district' => $row[5],
-            'price' => $row[6],
-            'description' => $row[7],
-            'purpose' => $row[8]
-        ]);
+        foreach ($collection as $row) {
+            if ($row->filter()->isNotEmpty()) {
+                Manure::firstOrCreate([
+                    'name' => $row[0],
+                    'norm_nitrogen' => $row[1],
+                    'norm_phosphorus' => $row[2],
+                    'norm_potassium' => $row[3],
+                    'culture_id' => $row[4],
+                    'district' => $row[5],
+                    'price' => $row[6],
+                    'description' => $row[7],
+                    'purpose' => $row[8]
+                ]);
+            }
+        }
     }
 }
